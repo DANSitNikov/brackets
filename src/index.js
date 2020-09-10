a4173abed4aec34c4d0c08b33785f239f6c93c5c
Daniil <sitnikovdan13@gmail.com>
2020-09-10 10:37:05 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (typeof str !== 'string' || typeof bracketsConfig !== 'object') {
+        return false;
+    }
+    let strRes = [],
+        arrRes = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].match(/\]|\[|\{|\}|\||\(|\)/)) {
+            strRes.push(str[i])
+        }
+    }
+    bracketsConfig = bracketsConfig.flat();
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i].match(/\]|\[|\{|\}|\||\(|\)/)) {
+            arrRes.push(bracketsConfig[i]);
+        }
+    }
+    if (strRes.length % 2 === 0 && arrRes.length % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
